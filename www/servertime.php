51e34ba0f0886b7519839c2119167c12e4aa428b
devpedz <jayrditona@gmail.com>
2024-11-09 14:34:32 +0800

    Update servertime.php

--- www/servertime.php
+++ www/servertime.php
@@ -1,6 +1,16 @@
 <?php
-// servertime.php
-date_default_timezone_set('Asia/Manila');
-// Get current server time
-$serverTime = new DateTime("now", new DateTimeZone('UTC')); // UTC timezone or any other timezone you need
-echo json_encode(['time' => $serverTime->format('c')]); // 'c' gives the ISO 8601 format
+header('Content-Type: application/json');
+
+// Get current time
+$use24HourClock = isset($_GET['use24HourClock']) ? (bool)$_GET['use24HourClock'] : true;
+$date = new DateTime();
+$hours = $use24HourClock ? $date->format('H') : $date->format('g');
+$minutes = $date->format('i');
+$seconds = $date->format('s');
+
+// Return the time as JSON
+echo json_encode([
+    'hours' => $hours,
+    'minutes' => $minutes,
+    'seconds' => $seconds
+]);
